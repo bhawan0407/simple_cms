734527e40ab1fb83b8b838f326d8f5df27a809ad
bhawan0407 <bhawandtu93@gmail.com>
2020-07-05 20:52:19 +0530

    Added relationships between models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,8 +1,12 @@
 class Subject < ApplicationRecord
 
+  has_many :pages
+
   scope :visible, -> { where(:visible => true) }
   scope :invisible, -> { where(:visible => false) }
   scope :sorted, -> { order("position ASC") }
   scope :newest, -> { order("created_at DESC") }
+
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
 
 end
