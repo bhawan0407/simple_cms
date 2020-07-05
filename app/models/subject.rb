73ffe522d4d2756bf08b1c18ec790b3babed03f5
bhawan0407 <bhawandtu93@gmail.com>
2020-07-05 19:18:40 +0530

    Added scopes in subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,8 @@
 class Subject < ApplicationRecord
+
+  scope :visible, -> { where(:visible => true) }
+  scope :invisible, -> { where(:visible => false) }
+  scope :sorted, -> { order("position ASC") }
+  scope :newest, -> { order("created_at DESC") }
+
 end
