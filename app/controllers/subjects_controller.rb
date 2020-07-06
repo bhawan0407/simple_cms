c04ce23ce1287251608bfc46d738c4f1b5cc7331
bhawan0407 <bhawandtu93@gmail.com>
2020-07-06 09:40:40 +0530

    Added subjects controller and routes

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,27 @@
+class SubjectsController < ApplicationController
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'subjects/edit'
+
+  get 'subjects/new'
+
+  get 'subjects/index'
+
+  get 'subjects/show'
+
+  get 'subjects/delete'
+
   get 'demo/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
